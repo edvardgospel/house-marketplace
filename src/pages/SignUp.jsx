6143912af26aa632b8873ee876bc0ab7d751c23d
edvardgospel <edvard.eros@yahoo.com>
2022-04-24 16:48:53 +0200

    Register User

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -18,6 +20,21 @@ const SignUp = () => {
             [e.target.id]: e.target.value
         }))
     }
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const auth = getAuth()
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+            const user = userCredential.user
+            updateProfile(auth.currentUser, {
+                displayName: name
+            })
+            navigate('/')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
 
         <>
@@ -27,7 +44,7 @@ const SignUp = () => {
                         Welcome Back!
                     </p>
                 </header>
-                <form>
+                <form onSubmit={onSubmit}>
                     <input type="text"
                         className="nameInput"
                         placeholder='Name'
